1566ec834653d508b5134b81a87023a92c8e055e
dirkkalmbach <Dirk Kalmbach>
2015-10-14 21:16:26 +0200

    Last Change before Submission

---
+++ Code/exploratory_analysis.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 21:48:32 2015
+
+@author: dirkkalmbach
+"""
+
+import sys
+import pickle
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from tester import test_classifier, dump_classifier_and_data
+import numpy as np
+import matplotlib.pyplot as plt
+import pprint
+features_list = ['poi','salary',"strength_of_email_conn_to_POI"]
+data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
+
+#--------------------------------->
+### Task 2: Remove outliers
+del data_dict['TOTAL']
+#<---------------------------------
+
+#--------------------------------->
+### Task 3: Create new feature(s)
+# 1. Create new feature for email-connection:
+def create_email_conn_feature(data_dict):
+	def email_conn(from_poi, to_poi, from_mess, to_mess):
+		if from_poi=="NaN" or to_poi=="NaN" or from_mess=="NaN" or to_mess=="NaN":
+			return 0
+		else: 
+			return (from_poi+to_poi)/float((from_mess+to_mess))
+	
+	for key in data_dict:
+		data_dict[key]["strength_of_email_conn_to_POI"] = email_conn(	
+				data_dict[key]["from_poi_to_this_person"],
+				data_dict[key]["from_this_person_to_poi"],
+				data_dict[key]["from_messages"],
+				data_dict[key]["to_messages"] )
+	return data_dict
+
+# 2. Fraction-Feature from Lesson 11-4
+def computeFraction( poi_messages, all_messages ):
+	fraction = 0.
+	if type(poi_messages)==int and type(all_messages)==int:
+		fraction = float(poi_messages) / all_messages
+	return fraction
+
+submit_dict = {}
+for name in data_dict:
+
+    data_point = data_dict[name]
+
+    from_poi_to_this_person = data_point["from_poi_to_this_person"]
+    to_messages = data_point["to_messages"]
+    fraction_from_poi = computeFraction( from_poi_to_this_person, to_messages )
+    data_dict[name]["fraction_from_poi"] = fraction_from_poi
+
+    from_this_person_to_poi = data_point["from_this_person_to_poi"]
+    from_messages = data_point["from_messages"]
+    fraction_to_poi = computeFraction( from_this_person_to_poi, from_messages )
+    data_dict[name]["fraction_to_poi"] = fraction_to_poi
+	
+# Use new feature:
+data_dict = create_email_conn_feature(data_dict)
+
+print "Number of persons: ", len(data_dict)
+print "Number of features: ", len(data_dict.items()[0][1])
+#<---------------------------------
+
+
+#--------------------------------->
+### convert data_dict to np.array
+#Create list of colnames:
+colnames=[]
+for i in data_dict.values()[0]:
+	colnames.append(i)
+# get poi first entry:
+i=colnames.index("poi")
+colnames[0], colnames[i] = colnames[i], colnames[0]
+j=colnames.index("email_address")
+colnames.pop(j) #del email-adress (problems with @)
+
+colnames.remove("from_messages")
+colnames.remove("to_messages")
+
+d = featureFormat(data_dict, colnames, sort_keys = True)
+l, f = targetFeatureSplit(d)
+#<---------------------------------
+
+
+#--------------------------------->
+print "------- SelectKBest --------"
+#Use sklearn for feature selection
+from sklearn.feature_selection import SelectKBest
+#selection = SelectKBest(k=3)
+#f = SelectKBest(k=5).fit_transform(f,l)
+t=SelectKBest(k=7)
+t.fit(f,l)
+
+print t.get_support() #shows selected features
+print colnames #to compare
+#<---------------------------------
+
+# Feature Importance
+print "------- Extra Tree --------"
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
+dataset = datasets.load_iris()
+# fit an Extra Trees model to the data
+model = ExtraTreesClassifier()
+model.fit(f, l)
+# display the relative importance of each attribute
+print(model.feature_importances_)
+
+
+
+# Recursive Feature Elimination
+print "------- Logistic Regression --------"
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+# load the iris datasets
+dataset = datasets.load_iris()
+# create a base classifier used to evaluate a subset of attributes
+model = LogisticRegression()
+# create the RFE model and select 3 attributes
+rfe = RFE(model, 3)
+rfe = rfe.fit(f,l)
+# summarize the selection of the attributes
+print(rfe.support_)
+print(rfe.ranking_)
+
+# to identify features print out features list (without poi)
+colnames.remove('poi')
+print colnames
+
+
+'''
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=5)
+
+
+
+test_classifier(clf, d, colnames)
+
+### Dump your classifier, dataset, and features_list so 
+### anyone can run/check your results.
+
+
+
+dump_classifier_and_data(clf, my_dataset, features_list)
+
+print "Done!"
+'''
+
+
+
+
+
+
+
+#data = featureFormat(data_dict, features_list, sort_keys = True)
+
+
+## FEATURE EXAMINATION
+def countNaN(a):
+	names_poi=[]
+	names_nonpoi=[]	
+	d={}
+	n_poi=0
+	n=0
+	for key in data_dict:
+		if  data_dict[key]['poi']==False:
+			if data_dict[key][a] == "NaN":
+				n+=1
+				names_nonpoi.append(key)
+			else:
+				if data_dict[key][a] == "NaN":
+					n_poi+=1
+					names_poi.append(key)
+	#d["NaNs_nonpoi"]=n
+	#d["NaNs_poi"]=n_poi
+	#d["POI":{"count":n_poi}]
+	d = {'POI': {'count': n_poi, 'names': names_poi}, 'NoPOI': {'count': n, 'names': names_nonpoi}}
+	return d	#, names_nonpoi, names_poi
+
+print countNaN('email_address')
+
+
+
+
+
+
+for i in data_dict.items()[0][1]:
+	print i
+
+
+
+
+
+
+
+
+
+# Plotting Outliers
+def PlotOutlier(f1,f2):
+	x=[]; y=[]; x_poi=[]; y_poi=[]
+	for key in data_dict:
+		if data_dict[key][f1]!="NaN" and data_dict[key][f2]!="NaN":
+			if data_dict[key]["poi"]==False:
+				x.append(data_dict[key][f1])
+				y.append(data_dict[key][f2])
+	for key in data_dict:
+		if data_dict[key][f1]!="NaN" and data_dict[key][f2]!="NaN":
+			if data_dict[key]["poi"]==True:
+				x_poi.append(data_dict[key][f1])
+				y_poi.append(data_dict[key][f2])	
+	p=plt.scatter(x_poi,y_poi,color='red')
+	n=plt.scatter(x,y,color='blue', alpha=0.5)
+	#plt.legend((p,n),("POI","No-POI"), loc="lower right")
+	plt.xlabel(f1)
+	plt.ylabel(f2)
+	plt.title("Plot to detect Outliers")	
+	sub_text = "No-POIs (n): " + str(len(x)) + "     POIs (n): " + str(len(x_poi))
+	plt.suptitle(sub_text)
+	#plt.annotate('local max', xy=(2, 1), xytext=(3, 1.5) )
+            
+            
+	plt.show()
+	print len(x_poi), len(y_poi)
+	print len(x), len(y)
+#PlotOutlier("strength_of_email_conn_to_POI",'shared_receipt_with_poi')
+PlotOutlier('salary', "bonus")
+
+'''
+
+
+
+
+
+
+'''
+# ab hier Code-Reste:
+
+
+import matplotlib.pyplot as plt
+from numpy.random import randn
+fig=plt.figure()
+ax1=fig.add_subplot(2,2,1)
+plt.plot(randn(50).cumsum(),'k--')
+
+x=[]; y=[]; c=[]
+for key in data_dict:
+	x.append(data_dict[key]["salary"])
+	y.append(data_dict[key]["bonus"])
+	c.append(data_dict[key]["poi"])
+plt.scatter(x,y)
+
+X = [1,2,3,4]
+Y1 = [4,8,12,16]
+Y2 = [1,4,9,16]
+plt.scatter(X,Y1,color='red')
+plt.scatter(X,Y2,color='blue')
+plt.show()
+
+def PlotOutlier(f1,f2):
+	x=[]; y=[]; x_poi=[]; y_poi=[]
+	for key in data_dict:
+		if data_dict[key][f1]!="NaN" and data_dict[key][f2]!="NaN":
+			if data_dict[key]["poi"]==False:
+				x.append(data_dict[key][f1])
+				y.append(data_dict[key][f2])
+	for key in data_dict:
+		if data_dict[key][f1]!="NaN" and data_dict[key][f2]!="NaN":
+			if data_dict[key]["poi"]==True:
+				x_poi.append(data_dict[key][f1])
+				y_poi.append(data_dict[key][f2])	
+	
+	
+	plt.scatter(x_poi,y_poi,color='red')
+	plt.scatter(x,y,color='blue')
+	plt.show()
+
+PlotOutlier("salary","bonus")
+
+
+plt.scatter(x,y,color='red')
+plt.scatter(x,c,color='blue')
+plt.show()
+
+
+## OUTLIER DETECTION
+from ggplot import *
+
+print ggplot(mtcars, aes('mpg', 'qsec')) + \
+  geom_point(colour='steelblue') + \
+  scale_x_continuous(breaks=[10,20,30],  \
+                     labels=["horrible", "ok", "awesome"])
+'''
---
+++ Code/poi_id.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+import sys
+import pickle
+from time import time
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from tester import test_classifier, dump_classifier_and_data
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+#features_list = ['poi','salary','bonus']
+#features_list = ['poi','salary','bonus',"strength_of_email_conn_to_POI",'restricted_stock_deferred'] # You will need to use more features
+#features_list = ['poi','salary',"bonus", 'restricted_stock_deferred',"fraction_to_poi", "fraction_from_poi"]
+#features_list = ['poi','salary',"bonus", "strength_of_email_conn_to_POI",'director_fees',"total_stock_value", "restricted_stock"]
+#features_list = ['poi', 'bonus', 'total_stock_value', 'salary', 'fraction_to_poi', 'exercised_stock_options','deferred_income','long_term_incentive']
+features_list = ['poi', 'bonus', 'total_stock_value', 'salary','deferred_income', "strength_of_email_conn_to_POI", 'exercised_stock_options','long_term_incentive']
+
+### Load the dictionary containing the dataset
+data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
+
+
+### Task 2: Remove outliers
+del data_dict['TOTAL']
+
+
+
+### Task 3: Create new feature(s)
+##########################################################
+# Feature Scaling-Function (if necessar):
+def feature_scaling(feature):
+	from sklearn import preprocessing
+	min_max_scaler = preprocessing.MinMaxScaler()
+	#min_max_scaler.fit_transform(value)
+	return min_max_scaler.fit_transform(feature)
+	
+# 1. Fraction-Feature from Lesson 11-4
+def computeFraction( poi_messages, all_messages ):
+	fraction = 0.
+	if type(poi_messages)==int and type(all_messages)==int:
+		fraction = float(poi_messages) / all_messages
+	return fraction
+
+submit_dict = {}
+for name in data_dict:
+
+    data_point = data_dict[name]
+
+    from_poi_to_this_person = data_point["from_poi_to_this_person"]
+    to_messages = data_point["to_messages"]
+    fraction_from_poi = computeFraction( from_poi_to_this_person, to_messages )
+    data_dict[name]["fraction_from_poi"] = fraction_from_poi
+
+    from_this_person_to_poi = data_point["from_this_person_to_poi"]
+    from_messages = data_point["from_messages"]
+    fraction_to_poi = computeFraction( from_this_person_to_poi, from_messages )
+    data_dict[name]["fraction_to_poi"] = fraction_to_poi
+
+
+# 2. Create new feature for email-connection:
+def create_email_conn_feature(data_dict):
+	def email_conn(from_poi, to_poi, from_mess, to_mess):
+		if from_poi=="NaN" or to_poi=="NaN" or from_mess=="NaN" or to_mess=="NaN":
+			return 0
+		else: 
+			return (from_poi+to_poi)/float((from_mess+to_mess))
+	
+	for key in data_dict:
+		data_dict[key]["strength_of_email_conn_to_POI"] = email_conn(	
+				data_dict[key]["from_poi_to_this_person"],
+				data_dict[key]["from_this_person_to_poi"],
+				data_dict[key]["from_messages"],
+				data_dict[key]["to_messages"] )
+	
+	return data_dict
+	
+# Use new feature:
+data_dict = create_email_conn_feature(data_dict)
+
+##########################################################
+
+
+
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+
+# Split the dataset in 70:30 (training : testing)
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=40)
+
+
+
+
+#I added this code snippet only for showing how validation can be done with 
+#KFold. For the project report I use the scores from tester.py.
+
+# Splitting using KFold
+
+#kfold
+from sklearn.cross_validation import KFold
+kf=KFold(len(labels), 3)
+for train_indices, test_indices in kf:
+	#make training and testing datasets
+	features_train = [features[ii] for ii in train_indices]
+	features_test = [features[ii] for ii in test_indices]
+	labels_train = [labels[ii] for ii in train_indices]
+	labels_test = [labels[ii] for ii in test_indices]
+
+# TRAIN DATA:
+t0 = time()
+
+# Pick one:
+clf = GaussianNB()
+#clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=2)
+#clf = RandomForestClassifier(min_samples_split=11,n_estimators=8)
+
+#clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),algorithm="SAMME", n_estimators=200)
+
+clf.fit(features_train, labels_train) # Train it on the training set
+print "training time:", round(time()-t0, 3), "s"
+
+# PREDICTION:
+pred = clf.predict(features_test)
+
+# VALIDATE MODEL:
+print "accuracy_score: ", metrics.accuracy_score(labels_test, pred)
+print "precision_score: ", metrics.precision_score(labels_test, pred) 
+print "recall_score: ", metrics.recall_score(labels_test, pred)
+print "f_score: ", metrics.f1_score(labels_test, pred)
+
+
+# Naive BAYES:
+#clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
+
+# DECISION TREE:
+#clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=2)
+
+
+# SVM:
+#clf = svm.SVC(kernel='rbf', C=10000)
+
+
+#Random Forest
+#clf = RandomForestClassifier(min_samples_split=11,n_estimators=6)
+
+
+# Adaboost
+#clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),algorithm="SAMME", n_estimators=200)
+																						
+
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script.
+### Because of the small size of the dataset, the script uses stratified
+### shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+test_classifier(clf, my_dataset, features_list)
+
+### Dump your classifier, dataset, and features_list so 
+### anyone can run/check your results.
+
+
+
+dump_classifier_and_data(clf, my_dataset, features_list)
+
+print "Done!"
